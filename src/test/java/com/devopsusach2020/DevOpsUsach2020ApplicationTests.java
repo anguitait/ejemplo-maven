3fdcbc2f22dcb84f75cca06d44fbaecca2ef051b
Luis Anguita <luis.anguita@solem.cl>
2022-01-13 21:44:05 -0300

    FIX:Test

--- src/test/java/com/devopsusach2020/DevOpsUsach2020ApplicationTests.java
+++ src/test/java/com/devopsusach2020/DevOpsUsach2020ApplicationTests.java
@@ -3,11 +3,14 @@ package com.devopsusach2020;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import junit.framework.Assert;
+
 @SpringBootTest
 class DevOpsUsach2020ApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assert.assertNull(null);
 	}
 
 }
